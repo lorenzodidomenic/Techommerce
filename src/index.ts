5889804c3c6922f1592ea3622552d6bdd05771cc
carlospata <SPTCRL02E26H163H@studium.unict.it>
2024-03-27 10:15:17 +0100

    chor: remove unuseless connection and adding product image

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,6 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', process.cwd() + '/src/views');
 
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'techommerce'
-});
-
 app.use('/', express.static('./src/views'));
 app.use('/shop', express.static('./src/views'));
 
